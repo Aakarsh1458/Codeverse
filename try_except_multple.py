597fce4af024a0578e83821dc78f8de2c436d7db
Aakarsh1458 <aakarsh1458@gmail.com>
2025-07-16 11:27:50 +0530

    Add files via upload

---
+++ try_except_multple.py
@@ -0,0 +1,9 @@
+try:
+    num=int(input("Enter a number"))
+    result=10/num
+except ZeroDivisionError:
+    print("Error:Division by zero is not allowed")
+except ValueError:
+    print("Error:Please enter a valid number.")
+else:
+    print(f"Result is {result}")
